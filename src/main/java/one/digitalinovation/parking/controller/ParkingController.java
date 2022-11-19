54029090387c24d237e200bc9d77e8152e45218b
andrea-junia <andre3aj.f@gmail.com>
2022-11-19 18:49:00 -0300

    primeiro endpoint de lista

--- src/main/java/one/digitalinovation/parking/controller/ParkingController.java
+++ src/main/java/one/digitalinovation/parking/controller/ParkingController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import one.digitalinovation.parking.controller.dto.ParkingDTO;
+import one.digitalinovation.parking.controller.mapper.ParkingMapper;
 import one.digitalinovation.parking.model.Parking;
 import one.digitalinovation.parking.service.ParkingService;
 
@@ -15,16 +17,20 @@ import one.digitalinovation.parking.service.ParkingService;
 public class ParkingController {
 
     private final ParkingService parkingService;    
+    private final ParkingMapper parkingMapper;
 
-    public ParkingController(ParkingService parkingService) {
+    public ParkingController(ParkingService parkingService, ParkingMapper parkingMapper) {
         this.parkingService = parkingService;
+        this.parkingMapper = parkingMapper;
     }
 
-
     @GetMapping
-    public List<Parking> findAll(){
+    public List<ParkingDTO> findAll(){
 
-        return parkingService.findAll();
+        List<Parking> parkingList = parkingService.findAll();
+        List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
+
+        return result;
 
     }
     
